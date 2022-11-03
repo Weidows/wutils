f18ade87b104f0c73c0f32b6be974d19301fc6db
Weidows <utsuko27@qq.com>
2022-11-03 15:55:46 +0800

    添加获取视频时长

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"os"
 	"testing"
 )
 
@@ -38,4 +39,16 @@ func TestMapToArray(t *testing.T) {
 func TestConditionalEqual(t *testing.T) {
 	fmt.Println(ConditionalEqual(true, 1, 2))
 	fmt.Println(ConditionalEqual(false, 1, 2))
+}
+
+func TestGetMP4Duration(t *testing.T) {
+	file, err := os.Open("video.mp4")
+	if err != nil {
+		panic(err)
+	}
+	duration, err := GetMP4Duration(file)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(duration)
 }
