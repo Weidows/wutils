d99a19ce1c64c8f7ad7c8048e043aa3e2d7484a3
Weidows <utsuko27@qq.com>
2023-11-30 03:17:56 +0800

    fix logger.

--- utils/log/log.go
+++ utils/log/log.go
@@ -6,22 +6,28 @@ import (
 )
 
 var (
-	log *logrus.Logger
+	logger = &logrus.Logger{
+		// 设置将日志输出到标准输出（默认的输出为stderr，标准错误）
+		// 日志消息输出可以是任意的io.writer类型
+		Out: os.Stdout,
+		//Formatter: new(logrus.TextFormatter),
+		Formatter: &logrus.JSONFormatter{
+			//TimestampFormat:   "yyyy-MM-dd HH:mm:ss.ffffff",
+			//TimestampFormat:   "HH.mm.ss",
+			DisableTimestamp:  false,
+			DisableHTMLEscape: false,
+			DataKey:           "",
+			FieldMap:          nil,
+			CallerPrettyfier:  nil,
+			PrettyPrint:       true,
+		},
+		Hooks: make(logrus.LevelHooks),
+		// 设置日志级别为warn以上
+		//Level: logrus.DebugLevel,
+		Level: logrus.WarnLevel,
+	}
 )
 
-func init() {
-	log = logrus.New()
-	// 设置日志格式为json格式
-	logrus.SetFormatter(&logrus.JSONFormatter{})
-
-	// 设置将日志输出到标准输出（默认的输出为stderr，标准错误）
-	// 日志消息输出可以是任意的io.writer类型
-	logrus.SetOutput(os.Stdout)
-
-	// 设置日志级别为warn以上
-	logrus.SetLevel(logrus.WarnLevel)
-}
-
 func GetLogger() *logrus.Logger {
-	return log
+	return logger
 }
